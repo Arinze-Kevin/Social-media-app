742c2f5031af4c5f3ef5a242c151142b727c21b9
Arinze-Kevin <egboguarinze@gmail.com>
2023-04-04 22:10:30 +0100

    first commit

---
+++ sanity-backend/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 't51jfsf8',
+    dataset: 'production'
+  }
+})
---
+++ sanity-backend/schemas/index.js
@@ -0,0 +1,5 @@
+export const schemaTypes = [post, user, comment, postedBy]
+import post from './post';
+import user from './user';
+import comment from './comment';
+import postedBy from './postedBy';
