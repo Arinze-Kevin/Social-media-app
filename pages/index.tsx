adb0b31c6e9f64cc30c275a0cf5621ee850d4e1a
Arinze-Kevin <egboguarinze@gmail.com>
2023-04-13 22:44:31 +0100

    Created VideoCard component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,25 @@
-import type { NextPage } from 'next'
 import axios from 'axios';
+import { Video } from '../types';
+import VideoCard from '../components/VideoCard';
+import NoResults from '../components/NoResults';
 
-// interface IProps {
-//   videos:
-// }
 
-const Home: NextPage = ({ videos }) => {
+interface IProps {
+  videos: Video[]
+}
+
+const Home = ({ videos }: IProps) => {
   console.log(videos);
 
   return (
-    <h1 className='text-3xl font-bold underline'>
-      Hello world!
-    </h1>
+    <div className='flex flex-col gap-10 videos h-full'>
+      {videos.length ? (videos.map((video: Video) => (
+        <VideoCard post={video} key={video._id} />
+      ))
+      ) : (
+        <NoResults text={'No Videos'} />
+      )}
+    </div>
   )
 }
 
