b7260eabca3fd8ee7bef81238cd22ef7845e8a0d
Arinze-Kevin <egboguarinze@gmail.com>
2023-04-24 22:15:29 +0100

    Created a dynamic id for fetching videos

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -16,7 +16,23 @@ interface IProps {
 
 const Detail = ({ postDetails }: IProps) => {
   return (
-    <div>Detail</div>
+    <div className='flex w-full absolute left-0 top-0 bg-white flex-wrap lg:flex-nowrap'>
+        <div className='relative flex-2 w-[1000px] lg:w-9/12 flex justify-center items-center bg-blurred-img bg-no-repeat bg-cover bg-center'>
+            <div className='absolute top-6 left-2 lg:left-6 flex gap-6 z-50'>
+                <p>
+                    <MdOutlineCancel className='text-white text-[35px]' />
+                </p>
+            </div>
+            <div className='relative'>
+                <div className='lg:h-[100vh] h-[60vh]'>
+                    <video 
+                      src={post.video.asset.url}
+                    >
+                    </video>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
