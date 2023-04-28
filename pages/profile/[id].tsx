23c844a15fc5c7c1f2cbc9a3110aa704b4ffd888
Arinze-Kevin <egboguarinze@gmail.com>
2023-04-28 16:04:49 +0100

    created user profile

--- pages/profile/[id].tsx
+++ pages/profile/[id].tsx
@@ -18,22 +18,22 @@ interface IProps {
 
 const Profile = ({ data }: IProps) => {
   const { user, userVideos, userLikedVideos } = data;
-  
+
     return (
         <div className="w-full">
             <div className="flex gap-6 md:gap-10 mb-4 bg-white w-full">
-              <div className='w-8 h-8'>
+              <div className='w-16 h-16 md:h-32'>
                  <Image
                    src={user.image}
-                   width={34}
-                   height={34}
+                   width={120}
+                   height={120}
                    className='rounded-full'
                    alt='user profile'
                    layout='responsive'
                  />
                </div>
-               <div className='hidden xl:block'>
-                 <p className='flex gap-1 items-center text-md font-bold text-primary lowercase'>
+               <div className='flex flex-col justify-center'>
+                 <p className='md:text-2xl tracking-wider flex gap-1 items-center justify-center text-md font-bold text-primary lowercase'>
                    {user.userName.replaceAll(' ', '')}
                    <GoVerified className='text-blue-400'/>
                  </p>
