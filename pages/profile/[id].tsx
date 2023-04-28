6f70d401bac576e6ec529e30b74691ec1b8b5f0c
Arinze-Kevin <egboguarinze@gmail.com>
2023-04-28 16:04:32 +0100

    created user profile

--- pages/profile/[id].tsx
+++ pages/profile/[id].tsx
@@ -8,9 +8,41 @@ import NoResults from "../../components/NoResults";
 import { IUser, Video } from "../../types";
 import { BASE_URL } from "../../utils";
 
-const Profile = () => {
+interface IProps {
+    data: {
+        user: IUser,
+        userVideos: Video[],
+        userLikedVideos: Video[]
+    }
+}
+
+const Profile = ({ data }: IProps) => {
+  const { user, userVideos, userLikedVideos } = data;
+  
     return (
-        <div>Profile</div>
+        <div className="w-full">
+            <div className="flex gap-6 md:gap-10 mb-4 bg-white w-full">
+              <div className='w-8 h-8'>
+                 <Image
+                   src={user.image}
+                   width={34}
+                   height={34}
+                   className='rounded-full'
+                   alt='user profile'
+                   layout='responsive'
+                 />
+               </div>
+               <div className='hidden xl:block'>
+                 <p className='flex gap-1 items-center text-md font-bold text-primary lowercase'>
+                   {user.userName.replaceAll(' ', '')}
+                   <GoVerified className='text-blue-400'/>
+                 </p>
+                 <p className='capitalize text-gray-400 text-xs'>
+                   {user.userName}
+                 </p>
+               </div>
+            </div>
+        </div>
     )
 }
 
@@ -19,7 +51,11 @@ export const getServerSideProps = async ({
 }: {
     params: { id: string }
 }) => {
-    const res = await axios.get(`${BASE_URL}/profile/${id}`)
+    const res = await axios.get(`${BASE_URL}/api/profile/${id}`)
+
+    return {
+        props: { data: res.data }
+    }
 }
 
 export default Profile;
